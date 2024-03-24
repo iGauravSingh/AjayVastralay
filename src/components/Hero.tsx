ab469716ae2882c13a3a5056fe60b2251195a9da
iGauravSingh <poetonroad@gmail.com>
2024-03-24 14:33:54 +0530

    resolving responsive image cut issue

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,14 +19,14 @@ const Hero = () => {
     <div className=' relative w-screen'>
         {/* image  */}
         <div className='w-full'>
-            <img className=' w-full h-[300px] md:h-[500px] object-cover ' src={heroData[heroDataState].image} alt="" />
+            <img className=' w-full h-[300px] md:h-[500px] object-left md:object-cover ' src={heroData[heroDataState].image} alt="hero" />
         </div>
 
         {/* image data  */}
         <div className=' absolute top-[50%] left-0 -translate-y-1/2 flex flex-col font-poppins gap-6 pl-4 md:pl-32 '>
             <div className=' flex flex-col gap-1 md:gap-2'>
-            <h2 className='text-3xl md:text-6xl'>{heroData[heroDataState].title1}</h2>
-            <h2 className='text-3xl'>{heroData[heroDataState].title2}</h2>
+            <h2 className='text-xl md:text-6xl'>{heroData[heroDataState].title1}</h2>
+            <h2 className='text-xl'>{heroData[heroDataState].title2}</h2>
             <p className=' text-sm'>{heroData[heroDataState].description}</p>
             </div>
             <Button text='Shop Now' />
